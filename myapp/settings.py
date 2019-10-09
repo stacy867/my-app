dd36dd420c4771a1a9a70636ec2e2d723502452f
stacy867 <stacymurenzi@gmail.com>
2019-10-09 15:48:34 +0200

    connected postgres with my application

--- myapp/settings.py
+++ myapp/settings.py
@@ -79,6 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER': 'wecode',
+    'PASSWORD':'190998st',
     }
 }
 
@@ -107,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
