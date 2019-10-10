d6f0d070d4ac303b9bd37e74ce16bcafacad049b
stacy867 <stacymurenzi@gmail.com>
2019-10-10 14:23:10 +0200

    changes

--- myapp/settings.py
+++ myapp/settings.py
@@ -77,8 +77,9 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'gallery',
         'USER': 'wecode',
     'PASSWORD':'190998st',
     }
