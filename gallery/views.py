7f44add899affa7e38c4c84362ea34f7747c284d
stacy867 <stacymurenzi@gmail.com>
2019-10-11 11:39:02 +0200

    added a function to search and display image

--- gallery/views.py
+++ gallery/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+from . models import Image,Location,Category
 
 # Create your views here.
 def welcome(request):
-    return render(request,'index.html')
+    
+    image=Image.objects.all()
+    return render(request,'index.html',{'image':image})
 
+def search_results(request):
+
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_by_name(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-apps/search.html',{"message":message,"images": searched_images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-apps/search.html',{"message":message})
+
+def display_image(request,image_id):
+    try:
+        image=Image.objects.get(id=image_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"welcome.html", {"image":image}) 
+            
