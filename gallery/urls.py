3166eea8c2825fccac75d56443d4ffd890d3c9e2
stacy867 <stacymurenzi@gmail.com>
2019-10-11 11:47:32 +0200

    added a path to view images

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,6 +1,13 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     url(r'^$',views.welcome,name = 'welcome'),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^image/(\d+)',views.display_image,name ='image'),
+
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
