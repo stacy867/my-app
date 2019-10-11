764f33c385e0dfef9d88661c5449df1ac2a719cb
stacy867 <stacymurenzi@gmail.com>
2019-10-11 14:27:04 +0200

    added a function to display categories

--- gallery/views.py
+++ gallery/views.py
@@ -9,10 +9,11 @@ def welcome(request):
     return render(request,'index.html',{'image':image})
 
 def search_results(request):
-
+    # print("searched_images")
     if 'image' in request.GET and request.GET["image"]:
         search_term = request.GET.get("image")
         searched_images = Image.search_by_name(search_term)
+        print("searched_images")
         message = f"{search_term}"
 
         return render(request, 'all-apps/search.html',{"message":message,"images": searched_images})
@@ -27,4 +28,13 @@ def display_image(request,image_id):
     except DoesNotExist:
         raise Http404()
     return render(request,"welcome.html", {"image":image}) 
+
+def display_category(request,name):
+    try:
+        category=Category.objects.filter(name=name).all()
+        image=Image.objects.all()
+        location=Location.objects.all()
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"category.html", {"image":image},{"category":category},{"location":location}) 
             
